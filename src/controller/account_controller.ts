e5399035771de0629b5d7a893a385a85cbc2d104
AbelYong <zs23014089@estudiantes.uv.mx>
2026-04-30 23:38:29 -0600

    update: get own account and social functions (follow, unfollow, get someone's followers)

--- src/controller/account_controller.ts
+++ src/controller/account_controller.ts
@@ -1,13 +1,35 @@
 import { Request, Response } from "express";
-import { AccountQueryInput, UpdateAccountInput } from "../schemas/account_schema.js";
+import { AccountParamsInput, UpdateAccountInput } from "../schemas/account_schema.js";
 import { db } from "../drizzle/db.js";
 import { Accounts } from "../drizzle/schema.js";
 import { eq } from "drizzle-orm";
 
-export const getAccountById = async (req: Request<{}, {}, {}, AccountQueryInput>, res: Response) : Promise<void> => {
-    const id = req.query["id"];
+export const getMyAccount = async (req: Request, res: Response) : Promise<void> => {
+    const userId = req.auth?.sub;
+
+    if (!userId) {
+        res.status(401).json({ message: "Invalid Token or subject is missing (sub)", code: "MISSING_SUB" });
+        return;
+    }
 
     const account = await db.query.Accounts.findFirst({
+        where: { id: userId }
+    });
+
+    if (account) {
+        res.status(200).json(account);
+    } else {
+        res.status(404).json({message: `No account matching for ${userId} was found`});
+    }
+}
+
+export const getAccountById = async (req: Request<AccountParamsInput, {}, {}>, res: Response) : Promise<void> => {
+    const id = req.params.id;
+
+    const account = await db.query.Accounts.findFirst({
+        columns: {
+            email: false
+        },
         where: { id: id }
     });
 
@@ -29,7 +51,7 @@ export const updateAccount = async (req: Request<{}, {}, UpdateAccountInput>, re
     const updateData = req.body;
 
     if (Object.keys(updateData).length === 0) {
-        res.status(400).json({ message: "No data to update was provided" });
+        res.status(400).json({ message: "No data to update was provided", code: "NO_CONTENT" });
         return;
     }
 
